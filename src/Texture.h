1ccc273653abb745ffb150947342c9ed1ca518f5
artem <12715139+artemkir@users.noreply.github.com>
2025-03-09 22:13:53 +0200

    Textures constructors and assigment operators improvements

--- src/Texture.h
+++ src/Texture.h
@@ -2,18 +2,32 @@
 
 class Texture : public ITexture
 {
-	SDL_Texture* texture;
+	SDL_Texture* texture = nullptr;
 
 public:
 
 	explicit Texture(SDL_Texture* tex) : texture(tex) {}
 
-	~Texture() final {
-		if (texture) {
-			SDL_DestroyTexture(texture);
-		}
-	}
+	~Texture() final { SDL_DestroyTexture(texture);	}
 
-	SDL_Texture* GetSDLTexture() const { return texture; }
+    // Deleted copy constructor and assignment operator to prevent accidental resource duplication.
+    Texture(const Texture&) = delete;
+    Texture& operator=(const Texture&) = delete;
+
+    // Implemented move constructor and assignment operator to safely transfer SDL_Texture ownership.
+    Texture(Texture&& other) noexcept : texture(other.texture) { other.texture = nullptr; }
+
+    Texture& operator=(Texture&& other) noexcept {
+        if (this != &other) {
+            if (texture) {
+                SDL_DestroyTexture(texture);
+            }
+            texture = other.texture;
+            other.texture = nullptr;
+        }
+        return *this;
+    }
+
+	SDL_Texture* GetSDLTexture() const noexcept { return texture; }
 
 };
